0d9e28f23cc4c8a819f94ce5aea73e06911acbd0
John Gerrard Holland <john_holland1@brown.edu>
2024-07-09 13:35:12 -0400

    reformat using black

--- src/interactive_engine/__main__.py
+++ src/interactive_engine/__main__.py
@@ -8,12 +8,13 @@ from typing import Sequence, List, Any, Optional, Union
 
 
 @dataclass
-class World():
+class World:
     landscape: str
     entities: List["Entity"]
 
+
 @dataclass
-class Entity():
+class Entity:
     name: str
     representation: str
     attributes: List[Union["Entity", dataclass]] = field(default_factory=list)
@@ -22,7 +23,9 @@ class Entity():
 
 def ability(f):
     """Decorator for abilities."""
-    first_argument_type = next(iter(inspect.signature(f).parameters.values())).annotation
+    first_argument_type = next(
+        iter(inspect.signature(f).parameters.values())
+    ).annotation
 
     def inner(e, *args, **kwargs):
         if isinstance(e, first_argument_type):
@@ -31,32 +34,41 @@ def ability(f):
             e.attributes = [inner(at, *args, **kwargs) for at in e.attributes]
         if hasattr(e, "entities"):
             e.attributes = [inner(e, *args, **kwargs) for e in e.entities]
+
     return inner
 
+
 @dataclass
-class Position():
+class Position:
     position: int
     world: World
 
+
 @ability
 def move(position: Position, displacement):
-    position.position = (position.position + displacement) % len(position.world.landscape)
+    position.position = (position.position + displacement) % len(
+        position.world.landscape
+    )
     return position
+
 
 @ability
 def move_plus_one(position: Position):
     position = move(position, 1)
     return position
 
+
 @ability
 def move_minus_one(position: Position):
     position = move(position, -1)
     return position
 
+
 @ability
 def random_walk(position: Position):
     position = random.choice([move_minus_one, move_plus_one])(position)
     return position
+
 
 def get_attributes(entity: Entity, kind: Any):
     return filter(lambda a: isinstance(a, kind), entity.attributes)
@@ -66,23 +78,29 @@ def render(gameworld: World) -> str:
     landscape = gameworld.landscape
     for entity in gameworld.entities:
         for p in get_attributes(entity, Position):
-            world_before = landscape[:p.position]
-            world_after = landscape[p.position+len(entity.representation):]
+            world_before = landscape[: p.position]
+            world_after = landscape[p.position + len(entity.representation) :]
             landscape = world_before + entity.representation + world_after
     return landscape
+
 
 def init_game():
     gameworld = World("_" * 15, [])
     robot_position = Position(0, gameworld)
-    robot = Entity("robot", "R", attributes=[robot_position, Position(2, gameworld)], abilities=[
-        random_walk])
-    gameworld.entities.extend([
-        robot,
-        Entity("cabinet", "C", attributes=[Position(5, gameworld)])
-    ])
+    robot = Entity(
+        "robot",
+        "R",
+        attributes=[robot_position, Position(2, gameworld)],
+        abilities=[random_walk],
+    )
+    gameworld.entities.extend(
+        [robot, Entity("cabinet", "C", attributes=[Position(5, gameworld)])]
+    )
+
     def tick():
         random_walk(robot)
         sleep(0.1)
+
     return gameworld, tick
 
 
@@ -94,6 +112,7 @@ def main_curses(stdscr, render=render):
         stdscr.clear()
         stdscr.addstr(render(gameworld))
         stdscr.refresh()
+
 
 if __name__ == "__main__":
     curses.wrapper(main_curses)
