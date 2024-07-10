ed13dc09e939bc9333206206c5476620109799f4
John Gerrard Holland <john_holland1@brown.edu>
2024-07-10 08:42:09 -0400

    add function to get all abilities (intrinsic and inherited) for an entity

--- src/interactive_engine/engine.py
+++ src/interactive_engine/engine.py
@@ -35,3 +35,48 @@ T = TypeVar("T")
 
 def get_attributes(entity: Entity, kind: Type[T]) -> Sequence[T]:
     return filter(lambda a: isinstance(a, kind), entity.attributes)
+
+
+def get_abilities(entity: Entity):
+    """
+    Examples:
+        >>> list(get_abilities(Entity()))
+        []
+        >>> @ability
+        ... def an_ability(target: Entity):
+        ...     return
+        >>> list(get_abilities(Entity(abilities=[an_ability])))  # doctest: +ELLIPSIS
+        [<function an_ability at 0x...>]
+
+        >>> @dataclass
+        ... class Container(Entity):
+        ...     contents: list = field(default_factory=list)
+        >>> def make_bag_of_holding():
+        ...     _container = Container()
+        ...     def insert(entity: Entity):
+        ...         return replace(_container, contents=_container.contents + [entity])
+        ...     def remove(entity: Entity):
+        ...         new_contents = _container.contents.copy().remove(entity)
+        ...         return replace(_container, contents=new_contents)
+        ...     bag_of_holding = Entity(attributes=[_container],
+        ...         abilities=[insert, remove])
+        ...     return bag_of_holding
+        >>> list(get_abilities(Entity(attributes=[make_bag_of_holding()])))
+        ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+        [<function make_bag_of_holding.<locals>.insert at 0x...>,
+         <function make_bag_of_holding.<locals>.remove at 0x...>]
+
+         >>> list(get_abilities(Entity(abilities=[an_ability], attributes=[make_bag_of_holding()])))
+         ... # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+         [<function an_ability at 0x...>,
+          <function make_bag_of_holding.<locals>.insert at 0x...>,
+          <function make_bag_of_holding.<locals>.remove at 0x...>]
+
+    """
+    if hasattr(entity, "abilities"):
+        for ability in entity.abilities:
+            yield ability
+    if hasattr(entity, "attributes"):
+        for attribute in entity.attributes:
+            for ability in get_abilities(attribute):
+                yield ability
