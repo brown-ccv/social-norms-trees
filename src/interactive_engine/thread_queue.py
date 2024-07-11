53519e39a1f350b4ea9b7582ef78df699d5d29e6
John Gerrard Holland <john_holland1@brown.edu>
2024-07-11 14:37:54 -0400

    update handling of multiple workers

--- src/interactive_engine/thread_queue.py
+++ src/interactive_engine/thread_queue.py
@@ -9,9 +9,9 @@ tasks = queue.Queue()
 def worker(id, message_queue):
     while True:
         item = tasks.get()
-        message_queue.put(f"{id=} start  {item=:2} {time.strftime('%X')}")
+        message_queue.put(f"{id=:2} start  {item=:2} {time.strftime('%X')}")
         time.sleep(random.choice([0.1, 0.1, 1, 2, 3]))
-        message_queue.put(f"{id=} finish {item=:2} {time.strftime('%X')}")
+        message_queue.put(f"{id=:2} finish {item=:2} {time.strftime('%X')}")
         tasks.task_done()
 
 
@@ -26,9 +26,8 @@ def reporter():
 
 
 # Turn-on the worker thread.
-threading.Thread(target=worker, daemon=True, args=(0, messages)).start()
-threading.Thread(target=worker, daemon=True, args=(1, messages)).start()
-threading.Thread(target=worker, daemon=True, args=(3, messages)).start()
+for i in range(10):
+    threading.Thread(target=worker, daemon=True, args=(i, messages)).start()
 threading.Thread(target=reporter, daemon=True).start()
 
 # Send thirty task requests to the worker.
