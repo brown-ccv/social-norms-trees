519b8c355b07efdd14e30fbd44db212383819fd4
John Gerrard Holland <john_holland1@brown.edu>
2024-07-11 15:03:53 -0400

    add more detailed tasks

--- src/interactive_engine/thread_queue.py
+++ src/interactive_engine/thread_queue.py
@@ -1,17 +1,29 @@
+import logging
 import random
 import threading
 import queue
 import time
+from dataclasses import dataclass
+from typing import Optional
 
 tasks = queue.Queue()
+_logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def worker(id, report):
     while True:
         item = tasks.get()
-        report(f"{id=:2} start  {item=:2} {time.strftime('%X')}")
-        time.sleep(random.choice([0.1, 0.1, 1, 2, 3]))
-        report(f"{id=:2} finish {item=:2} {time.strftime('%X')}")
+        _logger.debug(f"{id=:2} start  {item=} {time.strftime('%X')}")
+        match item:
+            case Task(kind="move to", value=place):
+                _logger.debug(f"{id=:2} moving to {place=}")
+                time.sleep(random.choice([0.1, 0.1, 1, 2, 3]))
+            case Task(kind="announce", value=message):
+                time.sleep(random.choice([0.1, 0.1, 1, 2, 3]))
+                report(f"{id=:2}: '{message}'")
+        _logger.debug(f"{id=:2} finish {item=} {time.strftime('%X')}")
         tasks.task_done()
 
 
@@ -34,15 +46,24 @@ _, reporter, reporter_callback = make_message_queue_and_reporter()
 threading.Thread(target=reporter, daemon=True).start()
 
 # Turn-on the worker thread.
-for i in range(10):
+for i in range(1):
     threading.Thread(
         target=worker, daemon=True, kwargs=dict(id=i, report=reporter_callback)
     ).start()
 
 
+@dataclass
+class Task:
+    kind: str
+    value: Optional[str] = None
+
+
 # Send thirty task requests to the worker.
-for item in range(100):
-    tasks.put(item)
+tasks.put(Task("move to", "A"))
+tasks.put(Task("move to", "B"))
+tasks.put(Task("move to", "C"))
+tasks.put(Task("announce", "I'm starting again"))
+tasks.put(Task("move to", "A"))
 
 # Block until all tasks are done.
 tasks.join()
