98bf6114e6fd58d5d33cb09e034f87d1481880b9
John Gerrard Holland <john_holland1@brown.edu>
2024-07-11 08:44:33 -0400

    add a patrol tree

--- norms/patrol.py
+++ norms/patrol.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from pprint import pprint
 
@@ -6,22 +7,40 @@ import py_trees
 from interactive_engine.engine import Entity
 from interactive_engine.world import LocationWorld
 
+world = LocationWorld()
+
+
+@dataclass
+class Location:
+    name: str
+
+
+for location in map(lambda x: Location(x), ["wetland", "prarie", "forest"]):
+    world.locations.append(location)
+
+
+travel_functions = []
+for location in world.locations:
+
+    def _function(self):
+        print(f"go to {location.name}")
+        if random.random() > 0.7:
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.RUNNING
+
+    _function.__name__ = f"go to {location.name}"
+    travel_functions.append(_function)
+
 tree = py_trees.composites.Sequence(
-    name="Sequence",
+    name="Patrol sequence",
     memory=False,
     children=[],
 )
 
-world = LocationWorld(locations=["wetland", "prarie", "forest"])
 
-
-for location in world.locations:
-
-    def _function():
-        print(f"go to {location}")
-
-    _function.__name__ = f"go to {location}"
-    tree.add_child(py_trees.meta.create_behaviour_from_function(_function)())
+for f in travel_functions:
+    tree.add_child(py_trees.meta.create_behaviour_from_function(f)())
 
 
 @dataclass
@@ -30,6 +49,14 @@ class PyTreesAgency:
 
 
 if __name__ == "__main__":
-    robot = Entity(name="", attributes=[PyTreesAgency(tree)])
+    robot = Entity(
+        name="",
+        attributes=[PyTreesAgency(tree), random.choice(world.locations)],
+    )
+
     pprint(robot)
+    print(py_trees.display.unicode_tree(tree))
+    tree.tick_once()
+    print(py_trees.display.unicode_tree(tree))
+    tree.tick_once()
     print(py_trees.display.unicode_tree(tree))
