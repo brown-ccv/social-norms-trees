bd4a88e43f40b37a68200eae8845f54b46250434
John Gerrard Holland <john_holland1@brown.edu>
2024-07-23 09:26:53 -0400

    add move node and exchange node functions

--- src/social_norms_trees/behaviortreeworld_chat.py
+++ src/social_norms_trees/behaviortreeworld_chat.py
@@ -137,10 +137,16 @@ def say(message):
 
 
 def prompt_identify_node(
-    tree: py_trees.behaviour.Behaviour, message: str = "Which node?"
+    tree: py_trees.behaviour.Behaviour,
+    message: str = "Which node?",
+    display_nodes: bool = True,
 ) -> py_trees.behaviour.Behaviour:
+    if display_nodes:
+        text = f"{format_tree_with_indices(tree)}\n{message}"
+    else:
+        text = f"{message}"
     node_index = click.prompt(
-        text=f"{format_tree_with_indices(tree)}\n{message}",
+        text=text,
         type=int,
     )
     node = next(islice(iterate_nodes(tree), node_index, node_index + 1))
@@ -221,6 +227,102 @@ def remove_node(tree: T, node: Optional[py_trees.behaviour.Behaviour] = None) ->
     return tree
 
 
+def move_node(
+    tree: T,
+    node: Optional[py_trees.behaviour.Behaviour] = None,
+    new_parent: Optional[py_trees.behaviour.Behaviour] = None,
+    index: int = None,
+) -> T:
+    """Exchange two behaviours in the tree
+
+    Examples:
+        >>> tree = py_trees.composites.Sequence("", False, children=[])
+
+    """
+
+    if node is None:
+        node = prompt_identify_node(tree, f"Which node do you want to move?")
+    if new_parent is None:
+        new_parent = prompt_identify_node(
+            tree, f"What should its parent be?", display_nodes=False
+        )
+    if index is None:
+        index = click.prompt(f"What should its position be?", type=int)
+
+    assert isinstance(new_parent, py_trees.composites.Composite)
+    assert isinstance(node.parent, py_trees.composites.Composite)
+
+    node.parent.remove_child(node)
+    new_parent.insert_child(node, index)
+
+    return tree
+
+
+def exchange_nodes(
+    tree: T,
+    node0: Optional[py_trees.behaviour.Behaviour] = None,
+    node1: Optional[py_trees.behaviour.Behaviour] = None,
+) -> T:
+    """Exchange two behaviours in the tree
+
+    Examples:
+        >>> tree = py_trees.composites.Sequence("", False, children=[
+        ...     s:=py_trees.behaviours.Success(),
+        ...     f:=py_trees.behaviours.Failure(),
+        ... ])
+        >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Success
+            --> Failure
+
+        >>> print(py_trees.display.ascii_tree(exchange_nodes(tree, s, f)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Failure
+            --> Success
+
+        >>> tree = py_trees.composites.Sequence("", False, children=[
+        ...     a:= py_trees.composites.Sequence("A", False, children=[
+        ...         py_trees.behaviours.Dummy()
+        ...     ]),
+        ...     py_trees.composites.Sequence("B", False, children=[
+        ...         py_trees.behaviours.Success(),
+        ...         c := py_trees.composites.Sequence("C", False, children=[])
+        ...     ])
+        ... ])
+        >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            [-] A
+                --> Dummy
+            [-] B
+                --> Success
+                [-] C
+        >>> print(py_trees.display.ascii_tree(exchange_nodes(tree, a, c)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            [-] C
+            [-] B
+                --> Success
+                [-] A
+                    --> Dummy
+    """
+
+    if node0 is None:
+        node0 = prompt_identify_node(tree, f"Which node do you want to switch?")
+    if node1 is None:
+        node1 = prompt_identify_node(
+            tree, f"Which node do you want to switch?", display_nodes=False
+        )
+
+    node0_parent, node0_index = node0.parent, node0.parent.children.index(node0)
+    node1_parent, node1_index = node1.parent, node1.parent.children.index(node1)
+
+    tree = move_node(tree, node0, node1_parent, node1_index)
+    tree = move_node(tree, node1, node0_parent, node0_index)
+
+    return tree
+
+
 if __name__ == "__main__":
 
     # main(world_)
@@ -253,7 +355,7 @@ if __name__ == "__main__":
 
     print(py_trees.display.ascii_tree(tree))
     print(format_tree_with_indices(tree))
-    remove_node(tree)
+    move_node(tree)
     print(format_tree_with_indices(tree))
     remove_node(tree)
     print(format_tree_with_indices(tree))
