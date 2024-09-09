19bc6b4689e5a6a79b6de3b08521eac50f563ac7
Josh <joshua_lu1@brown.edu>
2024-09-09 15:58:12 -0400

    update wording, fix some bugs

--- src/social_norms_trees/ui_wrapper.py
+++ src/social_norms_trees/ui_wrapper.py
@@ -12,17 +12,19 @@ from social_norms_trees.serialize_tree import serialize_tree, deserialize_tree
 DB_FILE = "db.json"
 
 
-def load_db():
-    if os.path.exists(DB_FILE):
-        with open(DB_FILE, "r") as f:
+def load_db(db_file):
+    if os.path.exists(db_file):
+        with open(db_file, "r") as f:
             return json.load(f)
     else:
         return {}
 
-def save_db(db):
+def save_db(db, db_file):
     """Saves the Python dictionary back to db.json."""
 
-    with open(DB_FILE, "w") as f:
+    print(f"\nWriting results of simulation to {db_file}...")
+
+    with open(db_file, "w") as f:
         json.dump(db, f, indent=4)
 
 def experiment_setup(db):
@@ -112,11 +114,11 @@ def initialize_experiment_record(db, participant_id, origin_tree):
 def run_experiment(db, participant_id, origin_tree, experiment_id):
 
     # Loop for the actual experiment part, which takes user input to decide which action to take
-    print("\nExperiment begins.\n")
+    print("\nExperiment beginning...\n")
 
-    run_simulation = True
-    while(run_simulation):
-
+    while(True):
+        
+        print(py_trees.display.ascii_tree(origin_tree))
         user_choice = click.prompt(
             "Would you like to perform an action on the behavior tree?",
             show_choices=True,
@@ -124,43 +126,50 @@ def run_experiment(db, participant_id, origin_tree, experiment_id):
         )
     
         if user_choice == 'y':
-            print("1. move node")
-            print("2. exchange node")
-            print("3. remove node")
-            action = click.prompt("Please select an action to perform on the behavior tree (enter the number)", type=int)
+            action = click.prompt(
+                "1. move node\n" +
+                "2. exchange node\n" + 
+                "3. remove node\n" + 
+                "Please select an action to perform on the behavior tree",
+                type=click.Choice(['1', '2', '3'], case_sensitive=False),
+                show_choices=True
+            )
 
-            if action == 1:
+            if action == "1":
                 db[experiment_id]["actions"].append("move node")
                 move_node(origin_tree)
-            elif action == 2:
+            elif action == "2":
                 db[experiment_id]["actions"].append("exchange node")
                 exchange_nodes(origin_tree)
-            elif action == 3:
+            elif action == "3":
                 db[experiment_id]["actions"].append("remove node")
                 remove_node(origin_tree)
             else:
-                print("Wrong choice, please enter correct number.\n")
+                print("Invalid choice, please select a valid number (1, 2, or 3).\n")
 
         else:
-            run_simulation = False
             db[experiment_id]["final_behavior_tree"] = serialize_tree(origin_tree)
             db[experiment_id]["end_date"] = datetime.now().isoformat()
-            print("\nSimulation has ended.")
+            break
+        
+    return db
    
 
 
 def main():
-    #TODO: load db from disc now. and each time program runs is 1 run of the program
     print("AIT Prototype #1 Simulator")
     
-    db = load_db()
+    DB_FILE = "db.json"
+    db = load_db(DB_FILE)
 
 
     participant_id, origin_tree, experiment_id = experiment_setup(db)
-    run_experiment(db, participant_id, origin_tree, experiment_id)
+    db = run_experiment(db, participant_id, origin_tree, experiment_id)
     
-    save_db(db)
+    save_db(db, DB_FILE)
     
+
+    print("\nSimulation has ended.")
 
     #TODO: visualize the differences between old and new behavior trees after experiment. 
     # Potentially use git diff 
