4f03035a5ada25933e25a9df4f3ff40e2ccd70b3
John Gerrard Holland <john_holland1@brown.edu>
2024-09-16 18:55:51 +0000

    finish up adding functions for mutating the nodes

--- src/social_norms_trees/mutate_ui.py
+++ src/social_norms_trees/mutate_ui.py
@@ -3,7 +3,8 @@ import py_trees
 
 T = TypeVar("T", bound=py_trees.behaviour.Behaviour)
 U = TypeVar("U", bound=py_trees.behaviour.Behaviour)
-C = TypeVar("C", bound=py_trees.behaviour.Composite)
+V = TypeVar("V", bound=py_trees.behaviour.Behaviour)
+C = TypeVar("C", bound=py_trees.composites.Composite)
 
 def remove_node(tree: T, node: U) -> Tuple[T, U]:
     """Remove a behaviour from the tree
@@ -34,7 +35,7 @@ def remove_node(tree: T, node: U) -> Tuple[T, U]:
     return tree, node
 
 
-def insert_node(tree: T, node: py_trees.behaviour.Behaviour, parent: C, index: int) -> T:
+def insert_node(tree: T, parent: C, index: int, node: py_trees.behaviour.Behaviour) -> T:
     """
     Examples:
         
@@ -45,13 +46,13 @@ def insert_node(tree: T, node: py_trees.behaviour.Behaviour, parent: C, index: i
         [-]
             --> Success
 
-        >>> print(py_trees.display.ascii_tree(insert_node(tree, py_trees.behaviours.Failure(), 1)))
+        >>> print(py_trees.display.ascii_tree(insert_node(tree, tree, 1, py_trees.behaviours.Failure())))
         ... # doctest: +NORMALIZE_WHITESPACE
         [-]
             --> Success
             --> Failure
 
-        >>> print(py_trees.display.ascii_tree(insert_node(tree, py_trees.behaviours.Dummy(), 0)))
+        >>> print(py_trees.display.ascii_tree(insert_node(tree, tree, 0, py_trees.behaviours.Dummy())))
         ... # doctest: +NORMALIZE_WHITESPACE
         [-]
             --> Dummy
@@ -59,50 +60,97 @@ def insert_node(tree: T, node: py_trees.behaviour.Behaviour, parent: C, index: i
             --> Failure
 
     """
-    parent.children.insert(index, node)
+    parent.insert_child(node, index)
     return tree
 
-# def move_node(
-#     tree: T,
-#     node: py_trees.behaviour.Behaviour,
-#     new_parent: py_trees.behaviour.Behaviour,
-#     index: int,
-# ) -> T:
-#     """Move a node in the tree
+def move_node(
+    tree: T,
+    parent: py_trees.behaviour.Behaviour,
+    index: int,
+    node: py_trees.behaviour.Behaviour,
+) -> T:
+    """Move a node in the tree
 
-#     Examples:
-#         >>> tree = py_trees.composites.Sequence("", False, children=[
-#         ...     failure_node := py_trees.behaviours.Failure(),
-#         ...     success_node := py_trees.behaviours.Success(),
-#         ... ])
-#         >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
-#         [-]
-#             --> Failure
-#             --> Success
+    Examples:
+        >>> tree = py_trees.composites.Sequence("", False, children=[
+        ...     failure_node := py_trees.behaviours.Failure(),
+        ...     success_node := py_trees.behaviours.Success(),
+        ... ])
+        >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Failure
+            --> Success
 
-#         >>> print(py_trees.display.ascii_tree(move_node(tree, failure_node, tree, 1)))
-#         ... # doctest: +NORMALIZE_WHITESPACE
-#         [-]
-#             --> Success
-#             --> Failure
+        >>> print(py_trees.display.ascii_tree(tree := move_node(tree, tree, 1, failure_node)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Success
+            --> Failure
+
+        >>> print(py_trees.display.ascii_tree(move_node(tree, tree, 1, failure_node)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Success
+            --> Failure
     
+    """
+    tree_without_node, removed_node = remove_node(tree, node)
+    tree_with_moved_node = insert_node(tree_without_node, parent, index, removed_node)
+    return tree_with_moved_node
 
-#     """
+def exchange_nodes(
+        tree: T,
+        node0: U,
+        node1: V,
+) -> T:
+    """Exchange two behaviours in the tree
 
-#     if node is None:
-#         node = prompt_identify_node(tree, f"Which node do you want to move?")
-#     if new_parent is None:
-#         new_parent = prompt_identify_parent_node(
-#             tree, f"What should its parent be?", display_nodes=True
-#         )
-#     if index is None:
-#         index = prompt_identify_child_index(new_parent)
+    Examples:
+        >>> tree = py_trees.composites.Sequence("", False, children=[
+        ...     s:=py_trees.behaviours.Success(),
+        ...     f:=py_trees.behaviours.Failure(),
+        ... ])
+        >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Success
+            --> Failure
 
-#     assert isinstance(new_parent, py_trees.composites.Composite)
-#     assert isinstance(node.parent, py_trees.composites.Composite)
+        >>> print(py_trees.display.ascii_tree(exchange_nodes(tree, s, f)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            --> Failure
+            --> Success
 
-#     # old_parent = node.parent.name
-#     node.parent.remove_child(node)
-#     new_parent.insert_child(node, index)
-        
-#     return tree
+        >>> tree = py_trees.composites.Sequence("", False, children=[
+        ...     a:= py_trees.composites.Sequence("A", False, children=[
+        ...         py_trees.behaviours.Dummy()
+        ...     ]),
+        ...     py_trees.composites.Sequence("B", False, children=[
+        ...         py_trees.behaviours.Success(),
+        ...         c := py_trees.composites.Sequence("C", False, children=[])
+        ...     ])
+        ... ])
+        >>> print(py_trees.display.ascii_tree(tree))  # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            [-] A
+                --> Dummy
+            [-] B
+                --> Success
+                [-] C
+        >>> print(py_trees.display.ascii_tree(exchange_nodes(tree, a, c)))
+        ... # doctest: +NORMALIZE_WHITESPACE
+        [-]
+            [-] C
+            [-] B
+                --> Success
+                [-] A
+                    --> Dummy
+    """
+
+    node0_parent, node0_index = node0.parent, node0.parent.children.index(node0)
+    node1_parent, node1_index = node1.parent, node1.parent.children.index(node1)
+
+    tree = move_node(tree, node1_parent, node1_index, node0)
+    tree = move_node(tree, node0_parent, node0_index, node1)
+
+    return tree
