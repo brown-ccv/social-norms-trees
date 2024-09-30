7459e0a39b108a4966d9976a8505c9b3561ab2a7
Josh <joshua_lu1@brown.edu>
2024-09-30 16:33:08 -0400

    outdated functions

--- src/social_norms_trees/behavior_library.py
+++ src/social_norms_trees/behavior_library.py
@@ -3,12 +3,3 @@ class BehaviorLibrary:
         self.behaviors = behavior_list 
         self.behavior_from_display_name = {behavior["display_name"]: behavior for behavior in behavior_list}
         self.behavior_from_id = {behavior["id"]: behavior for behavior in behavior_list}
-        
-    def get_behavior_by_display_name(self, display_name):
-        return self.behaviors.get(display_name)
-
-    def get_behavior_by_id(self, id_):
-        for behavior in self.behaviors.values():
-            if behavior.id == id_:
-                return behavior
-        return None
