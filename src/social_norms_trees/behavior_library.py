8717822de378d7464d6777e24bf3761d25e070d8
Josh <joshua_lu1@brown.edu>
2024-10-04 15:49:33 -0400

    reformat

--- src/social_norms_trees/behavior_library.py
+++ src/social_norms_trees/behavior_library.py
@@ -1,5 +1,7 @@
 class BehaviorLibrary:
     def __init__(self, behavior_list):
-        self.behaviors = behavior_list 
-        self.behavior_from_display_name = {behavior["display_name"]: behavior for behavior in behavior_list}
+        self.behaviors = behavior_list
+        self.behavior_from_display_name = {
+            behavior["display_name"]: behavior for behavior in behavior_list
+        }
         self.behavior_from_id = {behavior["id"]: behavior for behavior in behavior_list}
